704af73a49dd92540dd1ae8d7ee44ff0666c1e66
Berzeg07 <berezg86@gmail.com>
2017-01-17 22:37:27 +0300

    1

--- includes/header.php
+++ includes/header.php
@@ -1,2 +1,29 @@
-<a class="button-1 button-1_grey" href="#">Заказать обратный звонок</a>
-<input type="text" placeholder="8(--)">
+<section class="dd-top-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="dd-header-top-left">
+					<span>CASENOSKOV.RU</span><br>
+					<span>Кейс носков. Идеально.</span>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="karta-wrap">
+					<div class="dd-karta">
+						<span>Доставка по Всей России</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="dd-header-top-tel">
+					<span class="dd-sp1">Звоните<span> +7() ****????</span></span><br>
+					<span class="dd-sp2">время работы<span> 10:00 - 21:00</span></span>
+					<div class="dd-button-wrap">
+						<a class="button-1 button-1_grey button-1_font_1" href="#">Заказать обратный звонок</a>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</section>
