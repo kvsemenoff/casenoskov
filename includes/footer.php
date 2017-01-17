23cd4db77e387df604bc4c3c726a97870e5b38cd
unknown <anzarsh@mail.ru>
2017-01-17 17:33:10 +0300

    add 1

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<section class="section db-footer">
+<ul class="db-footer-ul">
+	<li><a href="#">Доставка и гарантия</a></li>
+	<li><a href="#">Контакты</a></li>
+	<li><a href="#">Оплата</a></li>
+	<li><a href="#">Шерстяные</a></li>
+	<li><a href="#">Хлопок 100%</a></li>
+	<li><a href="#">В кейсах</a></li>
+	<li><a href="#">Оптом</a></li>
+	<li><a href="#">От производителя</a></li>
+	<li><a href="#">По фасону</a></li>
+	<li><a href="#">Кейсы носков</a></li>
+	<li><a href="#">По цветам</a></li>
+	<li><a href="#">Носки на год</a></li>
+</ul>
+	
+</section>
