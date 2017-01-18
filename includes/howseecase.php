6447fd96119d124d85ff49b26c6dee43173f0098
unknown <anzarsh@mail.ru>
2017-01-18 14:53:34 +0300

    add 1

---
+++ includes/howseecase.php
@@ -0,0 +1,38 @@
+<section class="section section_background-db">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="center">
+					<h2 class="h2_white center_position-db">Как выглядит кейс?</h2>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+				<div class="col-md-4">
+					<div class="db-case-content__case1">
+						<img src="img/db-case.png" alt="img">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<ul class="db-case-content__check db-case-content__check_view">
+						<li>Солидно</li>
+						<li>Стильно</li>
+						<li>Качественно</li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<div class="db-case-content__case2">
+						<img src="img/db-case2.png" alt="img">
+					</div>
+				</div>	
+					
+				<div class="col-md-12">
+						<p class="db-text db-text_view">
+							От 30 до 100 пар носков упаковываются в черный лакированный кейс из твердого картона. <br>
+							Он будет удобен при переносе и отлично подойдет для хранения носков. <br>
+							Такой кейс носков внешне выглядит по-настоящему мужским подарком.
+						</p>
+				</div>
+				<div class="clearfix"></div>
+		</div>
+	</div>
+</section>
