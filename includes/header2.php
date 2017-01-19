be739ef39e99bd2247966b3fb4e0de8b13bd9cf4
Berzeg07 <berezg86@gmail.com>
2017-01-19 09:37:49 +0300

    29.14

---
+++ includes/header2.php
@@ -0,0 +1,45 @@
+
+<!-- Top header -->
+<section class="dd-top-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="dd-header-top-left">
+					<a href="#"><img class="img-responsive" src="img/logo.png" alt=""></a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="karta-wrap">
+					<div class="dd-karta">
+						<span>Доставка по Всей России</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="dd-header-top-tel">
+					<span class="dd-sp1">Звоните<span> +7() ****????</span></span><br>
+					<span class="dd-sp2">время работы<span> 10:00 - 21:00</span></span>
+					<div class="dd-button-wrap">
+						<a href="#log-in" name="modal" class="button-1 button-1_grey button-1_font_1">Заказать обратный звонок</a>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</section>
+
+<!-- Middle header -->
+<section class="dd-middle-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="middle-header-title">
+					<span>Купи КЕЙС НОСКОВ</span><br>
+					<span class="dd-next-span">по очень выгодной цене сегодня - и получи <span> 3 подарка!</span></span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
